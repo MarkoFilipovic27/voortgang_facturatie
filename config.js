05d75881c0cd85c3f789fd576ad6af74a8d87b20
Marko Filipović <markofilipo@hotmail.com>
2025-04-02 11:02:47 +0200

    Refactor: Move AFAS credentials to environment variables

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const AFAS_CONFIG = {
-    environmentKey: 'B4FD4BEF86DE42718D01C2C8069AFC52',
-    apiKey: 'EAD2F27CE9DE498DA091ECD46CD4A340',
-    token: '<token><version>1</version><data>8AB9EB5282A841BC9C4B80D1DA9E47E283C6652289E14E03AC52321138A24407</data></token>',
-    baseUrl: 'https://31219.restaccept.afas.online/ProfitRestServices/connectors',
-    environment: 'Accept'
+    environmentKey: process.env.AFAS_ENVIRONMENT_KEY,
+    apiKey: process.env.AFAS_API_KEY,
+    token: process.env.AFAS_TOKEN,
+    baseUrl: process.env.AFAS_BASE_URL || 'https://31219.restaccept.afas.online/ProfitRestServices/connectors',
+    environment: process.env.AFAS_ENVIRONMENT || 'Accept'
 }; 
+
+module.exports = AFAS_CONFIG; 
